8aa0bbe2cf8289b7e9697bcdfcb6fcd24e4daf6c
Inthrayuth Mahaphol and Nick Lyman <inthrayuth@>
2016-05-25 17:28:52 -0700

    Add 5 day forecast for F temps.

--- js/temperature-interface.js
+++ js/temperature-interface.js
@@ -2,6 +2,33 @@ var apiKey = require('./../.env').apiKey;
 var Weather = require('./../js/weather.js').Weather;
 
 $(document).ready(function(){
+
+  $('#fahrenheit').click(function(){
+    var city = $('#location').val();
+    $('#location').val("");
+    $('.showWeather').text("The city you have chosen is " + city + ".");
+
+    var requestedWeather = new Weather();
+
+    $.get(requestedWeather.getWeather(city)).then(function(response) {
+      requestedWeather.kelvin = response.main.temp;
+      $('.showWeather').text("The temperature in " + city + " is " + requestedWeather.fahrenheitConversion() + "° Fahrenheit.");
+    }).fail(function(error) {
+      $('.showWeather').text(error.responseJSON.message);
+    });
+
+    $.get(requestedWeather.getForecast(city)).then(function(response) {
+      $('.showForecast').append("The 5-day forecast for temperature in " + city + " is:");
+      for (var i = 0; i <= 4; i++) {
+        requestedWeather.kelvin = response.list[i].main.temp;
+        $('.showForecast').append("<li>" + "Day " + (i + 1) + ": " + requestedWeather.fahrenheitConversion() + "°F" + "</li>");
+      }
+    }).fail(function(error) {
+      $('.showForecast').text(error.responseJSON.message);
+    });
+  });
+
+
   $('#celcius').click(function(){
     var city = $('#location').val();
     $('#location').val("");
@@ -23,19 +50,7 @@ $(document).ready(function(){
         $('.showForecast').append("<li>" + "Day " + (i + 1) + ": " + requestedWeather.celciusConversion() + "°C" + "</li>");
       }
     }).fail(function(error) {
-      $('.showWeather').text(error.responseJSON.message);
-    });
-  });
-
-  $('#fahrenheit').click(function(){
-    var city = $('#location').val();
-    $('#location').val("");
-    $('.showWeather').text("The city you have chosen is " + city + ".");
-    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
-      var newWeather = new Weather(response.main.temp);
-      $('.showWeather').text("The temperature 5-day forecast in " + city + " is " + newWeather.fahrenheitConversion() + "° Fahrenheit.");
-    }).fail(function(error) {
-      $('.showWeather').text(error.responseJSON.message);
+      $('.showForecast').text(error.responseJSON.message);
     });
   });
 });
