0fe7e94d39aaf503e5a23bf9236ed02e1a87fa99
Inthrayuth Mahaphol and Nick Lyman <nick@>
2016-05-25 18:07:50 -0700

    Add comments to describe what steps are occurring.

--- js/temperature-interface.js
+++ js/temperature-interface.js
@@ -1,27 +1,30 @@
-var apiKey = require('./../.env').apiKey;
-var Weather = require('./../js/weather.js').Weather;
+var apiKey = require('./../.env').apiKey; //allows for use of API key (in hidden .env file) to be used
+var Weather = require('./../js/weather.js').Weather; //allows for Weather object to be imported and used (after being exported in weather.js)
 
 $(document).ready(function(){
 
   $('#fahrenheit').click(function(){
-    var city = $('#location').val();
-    $('#location').val("");
+    var city = $('#location').val(); //user inputted location from weather.html
+    $('#location').val(""); //clears the text entry after click
     $('.showWeather').text("The city you have chosen is " + city + ".");
 
-    var requestedWeather = new Weather();
+    var requestedWeather = new Weather(); //creates a new instance of Weather object
 
+    //gets the current temp from API call in weather.js and passes temp to kelvin property
     $.get(requestedWeather.getWeather(city)).then(function(response) {
       requestedWeather.kelvin = response.main.temp;
-      $('.showWeather').text("The temperature in " + city + " is " + requestedWeather.fahrenheitConversion() + "° Fahrenheit.");
+      $('.showWeather').text("The temperature in " + city + " is " + requestedWeather.fahrenheitConversion() + "° Fahrenheit."); //runs temp conversion to print accurate temp to page
     }).fail(function(error) {
-      $('.showWeather').text(error.responseJSON.message);
+      $('.showWeather').text(error.responseJSON.message); //returns error to user if API call is rejected
     });
 
+    //gets the forecasted temps from API call in weather.js and passes temp for each day to kelvin
     $.get(requestedWeather.getForecast(city)).then(function(response) {
       $('.showForecast').append("The 5-day forecast for temperature in " + city + " is:");
+      //loops through API array to pull out temps for 5 day forecast
       for (var i = 0; i <= 4; i++) {
         requestedWeather.kelvin = response.list[i].main.temp;
-        $('.showForecast').append("<li>" + "Day " + (i + 1) + ": " + requestedWeather.fahrenheitConversion() + "°F" + "</li>");
+        $('.showForecast').append("<li>" + "Day " + (i + 1) + ": " + requestedWeather.fahrenheitConversion() + "°F" + "</li>"); //runs temp conversion for each days temp to print correctly
       }
     }).fail(function(error) {
       $('.showForecast').text(error.responseJSON.message);
--- js/weather.js
+++ js/weather.js
@@ -1,25 +1,30 @@
-var apiKey = require('./../.env').apiKey;
+var apiKey = require('./../.env').apiKey; //allows for use of API key (in hidden .env file) to be used
 
 var Weather = function(kelvin) {
-  this.kelvin = kelvin;
+  this.kelvin = kelvin; //property of Weather object that holds temp from API
 };
 
+//temp converted to C and limited to 2 decimal places
 Weather.prototype.celciusConversion = function() {
   this.celcius = this.kelvin - 273.15;
   return this.celcius.toFixed(2);
 };
 
+//temp converted to F and limited to 2 decimal places
 Weather.prototype.fahrenheitConversion = function() {
   this.fahrenheit = (this.kelvin * (9/5) - 459.67);
   return this.fahrenheit.toFixed(2);
 };
 
+//method for returning current weather for user inputted location (city)
 Weather.prototype.getWeather = function(city) {
   return 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
 };
 
+//method for returning forecasted weather for user inputted location (city)
 Weather.prototype.getForecast = function(city) {
   return 'http://api.openweathermap.org/data/2.5/forecast?q=' + city + '&appid=' + apiKey;
 };
 
+//exporting is required for using Weather object in -interface.js files
 exports.Weather = Weather;
